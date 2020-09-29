f0fe213ec0250296a44061f7ccc5805fbbaa476c
vmkul <sanik133@gmail.com>
2020-09-29 14:38:41 +0300

    Almost working version

--- ABC/main.ts
+++ ABC/main.ts
@@ -3,16 +3,16 @@
 const WORKER_BEES = 5;
 const OBSERVER_BEES = 55;
 const VERTICES_COUNT = 300;
+const ITERATION_COUNT = 1000;
 
 const probability = 0.05;
-const AllColors = [];
-const UsedColors = [ 0 ];
+let UsedColors = [ 0 ];
 
 const printMatrix = (matrix: number[][]) => {
   matrix.map(arr => console.log(JSON.stringify(arr)));
 };
 
-const graphGen = (n : number) : number[][] => {
+const graphGen = (n: number): number[][] => {
   const res: number[][] = [];
   for (let i = 0; i < n; i++)
     res.push([]);
@@ -28,7 +28,7 @@ const graphGen = (n : number) : number[][] => {
   return res;
 };
 
-const checkGraph = (matrix: number[][]) : boolean => {
+const checkGraph = (matrix: number[][]): boolean => {
   let res = true;
   matrix.map(row => {
     const count: number = row.filter(n => n === 1).length;
@@ -38,26 +38,34 @@ const checkGraph = (matrix: number[][]) : boolean => {
 };
 
 class WorkerBee {
-  constructor(private matrix: number[][]) {}
+  constructor(private matrix: number[][]) { }
 
-  inspect(vertex: number) : number {
-    const power: number = this.matrix[vertex].filter(n => n === 1).length;
-    return power;
+  inspect(vertex: number): number[] {
+    const row = this.matrix[vertex];
+    const vertices = [];
+    for (let i = 0; i < row.length; i++) {
+      if (row[i] === 1)
+        vertices.push(i);
+    }
+    return vertices;
   }
 }
 
 class ObserverBee {
-  constructor(private matrix: number[][]) {}
+  constructor(private matrix: number[][]) { }
 
-  color(vertex: number, colors: Map<number, number>) : void {
-    let color = Math.floor(Math.random() * UsedColors.length);
+  color(vertex: number, colors: Map<number, number>): void {
+    const colorNumber = Math.floor(Math.random() * UsedColors.length);
+    let color = UsedColors[colorNumber];
     const row = this.matrix[vertex];
 
     let reserved = 0;
     for (let i = 0; i < row.length; i++) {
       if (row[i] === 1 && colors.get(i) === color) {
-        if (reserved === UsedColors.length)
-          UsedColors.push(UsedColors[UsedColors.length - 1] + 1);
+        if (reserved === UsedColors.length) {
+          color = Math.floor(Math.random() * 1000);
+          UsedColors.push(color);
+        }
         color = UsedColors[reserved++];
         i = 0;
       }
@@ -83,34 +91,55 @@ const observers: ObserverBee[] = [];
 for (let i = 0; i < WORKER_BEES; i++)
   workers.push(new WorkerBee(m));
 
-for (let i = 0; i < OBSERVER_BEES; i++)
-  observers.push(new ObserverBee(m));
-
 // Iteration
 
-const unvisited = [];
 const colors = new Map();
-const nectar = new Map();
-let totalNectar = 0;
+let bestChromatic = Infinity;
 
-for (let i = 0; i < VERTICES_COUNT; i++)
-  unvisited.push(i);
+for (let iter = 0; iter < ITERATION_COUNT; iter++) {
+  const unvisited = [];
+  const nectar = new Map();
 
-workers.forEach(worker => {
-  const vertex = Math.floor(Math.random() * unvisited.length);
-  unvisited.splice(unvisited.indexOf(vertex), 1);
-  const value = worker.inspect(vertex);
-  totalNectar += value;
-  nectar.set(vertex, value);
-});
+  for (let i = 0; i < VERTICES_COUNT; i++)
+    unvisited.push(i);
 
-console.log(totalNectar);
-console.log(nectar);
-console.log(unvisited.length);
+  while (unvisited.length !== 0) {
+    for (let i = 0; i < OBSERVER_BEES; i++)
+      observers.push(new ObserverBee(m));
 
-for (let i = 0; i < 300; i++) {
-  observers[0].color(i, colors);
+    workers.forEach(worker => {
+      const vertex = Math.floor(Math.random() * unvisited.length);
+      unvisited.splice(unvisited.indexOf(vertex), 1);
+      const value = worker.inspect(vertex);
+      nectar.set(vertex, value);
+    });
+
+    let totalNectar = 0;
+    for (const n of nectar.values()) 
+      totalNectar += n.length;
+
+    for (const v of nectar.keys()) {
+      observers.pop().color(v, colors);
+    }
+
+    for (const nectarVal of nectar.values()) {
+      const pi = nectarVal.length / totalNectar;
+      let count = pi * (OBSERVER_BEES - WORKER_BEES) - 1;
+      nectarVal.forEach(v => {
+        if (count-- > 0)
+          observers.pop().color(v, colors);
+      });
+    }
+
+    nectar.clear();
+  }
+
+  const uniqueColors = new Set(colors.values());
+  UsedColors = Array.from(uniqueColors);
+  bestChromatic = Math.min(UsedColors.length, bestChromatic);
+
+  if (iter % 20 === 0) {
+    console.log(`Chromatic number: ${bestChromatic} (${UsedColors.length})`);
+  }
 }
 
-console.log(colors);
-console.log(UsedColors);
